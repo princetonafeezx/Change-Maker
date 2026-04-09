64d156c35628660735a6e8677ad38b2f598d3e03
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-08 17:59:41 -0700

    Define the denominations of the currency

--- change_maker.py
+++ change_maker.py
@@ -5,3 +5,16 @@ from typing import cast
 
 from schemas import ChangeResult, GreedyTraceStep, ParsedAmountToCents
 from storage import format_money
+
+DENOMINATIONS: dict[int, dict[str, str]] = {
+    10000: {"name": "$100 bill", "type": "bill"},
+    5000: {"name": "$50 bill", "type": "bill"},
+    2000: {"name": "$20 bill", "type": "bill"},
+    1000: {"name": "$10 bill", "type": "bill"},
+    500: {"name": "$5 bill", "type": "bill"},
+    100: {"name": "$1 bill", "type": "bill"},
+    25: {"name": "quarter", "type": "coin"},
+    10: {"name": "dime", "type": "coin"},
+    5: {"name": "nickel", "type": "coin"},
+    1: {"name": "penny", "type": "coin"},
+}
